42f9f202f73f5d2d6f3716f660296330d0a0c2ca
Starxy <galaxy_djx@hotmail.com>
2018-12-13 14:31:46 +0800

    [update] 修改文件结构

--- run.py
+++ run.py
@@ -1,17 +1,13 @@
-import sys
-
-sys.path.append('.')
-
 from multiprocessing import Process
-from Eureka.Api.api import api_run as ApiRun
-from Eureka.Schedule.DeckInfoRefresh import run as RefreshRun
+from Eureka.Api.api import api_run as api_run
+from Eureka.Schedule.DeckInfoRefresh import run as refresh_run
 
 
 def run():
     p_list = list()
-    p1 = Process(target=ApiRun, name='ApiRun')
+    p1 = Process(target=api_run, name='ApiRun')
     p_list.append(p1)
-    p2 = Process(target=RefreshRun, name='RefreshRun')
+    p2 = Process(target=refresh_run, name='RefreshRun')
     p_list.append(p2)
 
     for p in p_list:
